6a618b8b26bf6f3090f41da523783aeaef10b06c
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2024-08-22 18:06:21 +0800

    change Price block

--- src/components/Main/Price/Price.tsx
+++ src/components/Main/Price/Price.tsx
@@ -1,32 +1,109 @@
 import * as React from "react";
 import styles from "./index.module.scss";
 import { PriceCard } from "./PriceCard/PriceCard";
+import Image from "next/image";
+import { Button } from "@/components/ui/buttons/Button";
+import { motion } from "framer-motion";
 
 export function Price() {
+  const variants = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0 },
+  };
+
   return (
     <div id="online-training" className={styles.price}>
       <h1 className={styles.title}>
         Сколько Вы готовы тратить на тренировочный процесс в месяц?
       </h1>
       <div className={styles.cards}>
-        <PriceCard
-          title="Базовый"
-          image="/images/price/price1.jpg"
-          price={5000}
-          text="Инфо про тариф"
-        />
-        <PriceCard
-          title="Лучший друг"
-          image="/images/price/price2.jpg"
-          price={10000}
-          text="Инфо про тариф"
-        />
-        <PriceCard
-          title="All Inclusive"
-          image="/images/price/price3.jpg"
-          price={15000}
-          text="Инфо про тариф"
-        />
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 0.3 }}
+          variants={variants}
+          className={styles.priceCard}
+        >
+          <div className={styles.content}>
+            <div className={styles.heading}>
+              <Image
+                src="/images/price/price1.jpg"
+                alt="Price card"
+                width={100}
+                height={100}
+              />
+              <h4 className={styles.title}>Тариф Базовый</h4>
+            </div>
+            <div className={styles.description}>
+              <p className={styles.text}>Инфо про тариф</p>
+              <span className={styles.price}>
+                Цена - {Number(5000).toLocaleString("de-DE")} руб./мес.
+              </span>
+            </div>
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button className={styles.buy}>Купить</Button>
+            <Button className={styles.more}>Подробнее</Button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 0.3 }}
+          variants={variants}
+          className={styles.priceCard}
+        >
+          <div className={styles.content}>
+            <div className={styles.heading}>
+              <Image
+                src="/images/price/price2.jpg"
+                alt="Price card"
+                width={100}
+                height={100}
+              />
+              <h4 className={styles.title}>Тариф Лучший друг</h4>
+            </div>
+            <div className={styles.description}>
+              <p className={styles.text}>Инфо про тариф</p>
+              <span className={styles.price}>
+                Цена - {Number(10000).toLocaleString("de-DE")} руб./мес.
+              </span>
+            </div>
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button className={styles.buy}>Купить</Button>
+            <Button className={styles.more}>Подробнее</Button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 0.3 }}
+          variants={variants}
+          className={styles.priceCard}
+        >
+          <div className={styles.content}>
+            <div className={styles.heading}>
+              <Image
+                src="/images/price/price3.jpg"
+                alt="Price card"
+                width={100}
+                height={100}
+              />
+              <h4 className={styles.title}>Тариф All Inclusive</h4>
+            </div>
+            <div className={styles.description}>
+              <p className={styles.text}>Инфо про тариф</p>
+              <span className={styles.price}>
+                Цена - {Number(15000).toLocaleString("de-DE")} руб./мес.
+              </span>
+            </div>
+          </div>
+          <div className={styles.buttonGroup}>
+            <Button className={styles.buy}>Купить</Button>
+            <Button className={styles.more}>Подробнее</Button>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
