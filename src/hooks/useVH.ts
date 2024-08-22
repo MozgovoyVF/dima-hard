78fbafc6a9562b0382f4ebf9331fc209b7e3ab46
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2024-08-22 19:16:35 +0800

    fix ios problem

--- src/hooks/useVH.ts
+++ src/hooks/useVH.ts
@@ -1,8 +1,20 @@
 import { useEffect } from "react";
 
 export const useVH = () => {
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    // let vh = window.innerHeight * 0.01;
+    // console.log("change VH");
+    // document.documentElement.style.setProperty("--vh", `${vh}px`);
+    window.addEventListener("resize", appHeight);
+    appHeight();
+
+    return () => {
+      document.removeEventListener("resize", appHeight);
+    };
   }, []);
-}
+};
